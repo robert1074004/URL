efebc96c4f01889518fae975d4e16ebc7f470aa4
robert1074004 <abc50113@yahoo.com.tw>
2022-05-16 17:21:54 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://root:abc83213@learning.lmzd7.mongodb.net/URL?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
 const app = express()
+
+const db = mongoose.connection
+
+db.on('error',() => {
+    console.log('mongodb error!')
+})
+
+db.once('open',() => {
+    console.log('mongodb connected')
+})
 
 app.get('/',(req,res) => {
     res.send('hello world')
