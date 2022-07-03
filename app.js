648c3cfcdcca7d2a77baa212e370e5a246f81ce9
robert1074004 <abc50113@yahoo.com.tw>
2022-07-03 15:52:29 +0800

    modify app.js & end.hbs

--- app.js
+++ app.js
@@ -1,97 +1,107 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const URL = require('./URL/url')
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+const URLS = require('./URL/url')
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 const app = express()
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
-const { redirect } = require('express/lib/response')
+const {
+    redirect
+} = require('express/lib/response')
 
 const lower = 'abcdefghijklmnopqrstuvwxyz'.split("")
 const upper = lower.map(i => i.toUpperCase())
 const number = '0123456789'.split("")
-let collection = lower.concat(upper,number)
+let collection = lower.concat(upper, number)
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 const db = mongoose.connection
 
-db.on('error',() => {
+db.on('error', () => {
     console.log('mongodb error!')
 })
 
-db.once('open',() => {
+db.once('open', () => {
     console.log('mongodb connected')
 })
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/error',(req,res) => {
+app.get('/error', (req, res) => {
     res.render('error')
 })
 
-app.post('/',(req,res) => {
+app.post('/', (req, res) => {
     const url = req.body.url
-    // 檢查url是否為空白
-    if (url.trim() === '') {
-        res.redirect('error')
-    } else {
-        URL.find().lean()
-        .then(urls => {
-            // 檢查url是否重複
-            let repeat = urls.some(URL => URL.URL === url)
-            // 如果重複
-            if (repeat) {
-                let information = urls.find(URL => URL.URL === url)
-                res.redirect(`/end/${information._id}`)
-            // 如果沒重複
-            } else { 
-                let Number = ""
-                for (let i = 0 ; i<5 ; i++){
-                    Number += collection[Math.floor(Math.random()*collection.length)]
-                }
-                const newURL = 'http://localhost:3000/'+Number
-                return URL.create({newURL:newURL,URL:url})
-                    .then(() =>  {
-                        URL.find().lean()
-                        .then(urls => {
-                            let information = urls.find(url => url.newURL === newURL)
-                            res.redirect(`/end/${information._id}`)
-                        }   
-                            )
-                        })
-                    .catch(error => console.log(error))
-            }
-        })
-        .catch(error => console.log(error))
+    // 檢查url是否為空白或不符合網址格式
+    if (url.trim() === '' || !url.includes('https://')) {
+        return res.redirect('error')
     }
+    // 檢查url是否重複
+    URLS.findOne({
+            URL: url
+        })
+        // 找得到資料，代表url已重複
+        .then(url => {
+            return res.redirect(`/end/${url._id}`)
+        })
+        // 找不到資料，新增新的url到資料庫
+        .catch(() => {
+            let Numbers = ""
+            for (let i = 0; i < 5; i++) {
+                Numbers += collection[Math.floor(Math.random() * collection.length)]
+            }
+            const newURL = Numbers
+            URLS.create({
+                    newURL: newURL,
+                    URL: url
+                })
+                .then(newurl => {
+                    URLS.findOne({
+                            URL: newurl.URL
+                        })
+                        .then(url => {
+                            res.redirect(`/end/${url._id}`)
+                        })
+                })
+        })
 })
 
-app.get('/end/:id',(req,res) => {
+
+app.get('/end/:id', (req, res) => {
     const id = req.params.id
-    return URL.findById(id)
-                .lean()
-                .then(url =>  res.render('end',{url}))
-                .catch(error => console.log(error))       
-})
-
-app.get('/:newURL',(req,res) => {
-    const newURL = 'http://localhost:3000/'+req.params.newURL
-    URL.find()
+    return URLS.findById(id)
         .lean()
-        .then(urls => {
-            let information = urls.find(url => url.newURL === newURL)
-            res.redirect(`${information.URL}`,302)
-        } )
-        .catch(error => console.log(error))          
+        .then(url => res.render('end', {
+            url
+        }))
+        .catch(error => console.log(error))
+})
+
+app.get('/:newURL', (req, res) => {
+    const newURL = req.params.newURL
+    URLS.findOne({
+            newURL
+        })
+        .then(url => res.redirect(`${url.URL}`, 302))
+        .catch(error => console.log(error))
 })
 
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
